3ea5b80ab7bfb03505613ff8011f045f9c6933d4
Tocoolmh58 <tobias.m.wild@gmail.com>
2021-11-18 11:47:38 +0100

    fix description

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ export default function Home() {
             <Head>
                 <title>Tobias Wild | Portfolio</title>
                 <meta
-                    description="I am a 16 years old student from Germany.
+                    name="description"
+                    content="I am a 16 years old student from Germany.
                         I love coding. I started learning HTML and CSS in mid
                         2018 when my school offered a differentiation course on
                         it. And from there on I taught my self JS. In mid-2021,
@@ -32,7 +33,7 @@ export default function Home() {
                     Hey, I&apos;m a full-stack developer based in Germany!
                 </div>
                 <article className="pt-0">
-                    <div className="float-right tablet:ml-6 rounded-full max-h-32 w-32 p-1 bg-gradient-to-br from-[#102e97] to-pink-500">
+                    <div className="float-right tablet:ml-6 rounded-full max-h-32 w-32 p-1 bg-gradient-to-br from-blue-700 to-pink-500">
                         <Image
                             src="/images/pexels-soumil-kumar-735911.jpg"
                             alt="headerImage"
@@ -43,12 +44,12 @@ export default function Home() {
                     </div>
                     <h1 className="mt-4">Tobias Wild</h1>
                     <p>
-                        I am a <span id="age" /> years old student from Germany.
-                        I love coding. I started learning HTML and CSS in mid
-                        2018 when my school offered a differentiation course on
-                        it. And from there on I taught my self JS. In mid-2021,
-                        I took an online course on web development with ReactJS.
-                        Then I started learning and using NextJS.
+                        I am a <span id="age">16</span> years old student from
+                        Germany. I love coding. I started learning HTML and CSS
+                        in mid 2018 when my school offered a differentiation
+                        course on it. And from there on I taught my self JS. In
+                        mid-2021, I took an online course on web development
+                        with ReactJS. Then I started learning and using NextJS.
                     </p>
                 </article>
                 <Projects />
