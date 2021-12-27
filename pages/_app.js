6ebc5269c7fd824228a533ecde04ec5f2e87de3f
Tocoolmh58 <tobias.m.wild@gmail.com>
2021-12-27 01:09:14 +0100

    framer-motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import 'Styles/globals.css'
+import { motion, AnimatePresence } from 'framer-motion'
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+    return (
+        <AnimatePresence>
+            <motion.div
+                key={router.route}
+                initial="pageInitial"
+                animate="pageAnimate"
+                exit="pageExit"
+                variants={{
+                    pageInitial: {
+                        opacity: 0,
+                    },
+                    pageAnimate: {
+                        opacity: 1,
+                    },
+                    pageExit: {
+                        opacity: 0,
+                    },
+                }}
+                transition={{ duration: 0.4 }}>
+                <Component {...pageProps} />
+            </motion.div>
+        </AnimatePresence>
+    )
 }
 
 export default MyApp
